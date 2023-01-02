4d605c492eb7858feab952c34df90db8e632e142
Marshall Thompson <marshall@creativeideal.net>
2023-01-01 23:56:06 -0700

    feat: expose pinia to exports

--- src/modules/pinia.ts
+++ src/modules/pinia.ts
@@ -1,10 +1,11 @@
 import { createPinia } from 'pinia'
 import { type UserModule } from '~/types'
 
+export const pinia = createPinia()
+
 // Setup Pinia
 // https://pinia.vuejs.org/
 export const install: UserModule = ({ isClient, initialState, app }) => {
-  const pinia = createPinia()
   app.use(pinia)
   // Refer to
   // https://github.com/antfu/vite-ssg/blob/main/README.md#state-serialization
